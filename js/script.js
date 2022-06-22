5779c8679fcef2aebf8ec78af91e2db192649cdb
simonebonifazi <simonbonifraz@gmail.com>
2022-06-22 19:07:42 +0200

    added-part-of-click-eventListener

--- js/script.js
+++ js/script.js
@@ -10,3 +10,51 @@ Aggiungere stile in modo da avere una bella pagina per la nostra compagnia di tr
 Potete usare quel che vi pare (Bootstrap, bool12 , CSS plain)
 Se volete potete anche spingervi al responsive ma l'importante è che facciate funzionare il form e stampare correttamente i dati del biglietto!
 */
+
+//dichiaro le variabili del form
+const nameSurname = document.getElementById('name-and-surname')
+const kms = document.getElementById('km')
+const userAge = document.getElementById('age')
+const button = document.getElementById('discover-price')
+
+// attivo il bottone al click
+button.addEventListener('click', function () {
+    const name = nameSurname.value;
+    console.log(name)
+
+    const km = kms.value;
+    console.log(km)
+
+    const age = userAge.value;
+    console.log(age)
+
+    // calcolo il prezzo del biglietto
+    const ticketPrice = 0.21 * km
+    console.log(ticketPrice)
+
+    let discountedPrice
+
+    if (!isNaN(km) && (km > 0) && (!isNaN(age) && (age > 0))) {
+
+
+        if (age < 18) {
+
+            discountedPrice = (ticketPrice * 0.8).toFixed(2)
+
+        }
+
+        else if (age > 65) {
+            discountedPrice = (ticketPrice * 0.6).toFixed(2)
+        }
+
+        else {
+            discountedPrice = (ticketPrice).toFixed(2)
+        };
+
+
+    } else {
+        alert("Errore. Esprimi le risposte in numeri positivi!")
+
+    }
+
+})
