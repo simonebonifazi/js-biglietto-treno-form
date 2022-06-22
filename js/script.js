8e04598c2bee5ff79a135a3aa150d3e47e6974eb
simonebonifazi <simonbonifraz@gmail.com>
2022-06-22 18:04:33 +0200

    added-mark-up

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Il programma dovrà mostrare un form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+Bonus:
+Aggiungere stile in modo da avere una bella pagina per la nostra compagnia di treni. Allego un'immagine di riferimento per chi non ha fantasia. :occhiolino:
+Potete usare quel che vi pare (Bootstrap, bool12 , CSS plain)
+Se volete potete anche spingervi al responsive ma l'importante è che facciate funzionare il form e stampare correttamente i dati del biglietto!
+*/
